27c210b45d284be6c9d53f75aa4c1045dc240e29
DouglasOttoSf <douglas.davila@objectedge.com>
2023-01-26 00:14:51 -0300

    Updating code

--- test/data/dailyTasks.js
+++ test/data/dailyTasks.js
@@ -1,7 +1,9 @@
+const genericDescription = `Daily Check-in | General project discussion | Ticket's QA`;
+
 export const dailyTasks = [
-    [8, "Daily Check-in | General project discussion | Documentation analysis"], //monday
-    [8, "Daily Check-in | General project discussion | Documentation analysis | Catalog Specialized"], //tuesday
-    [8, "Daily Check-in | Documentation analysis | Search v2/Multisite Estimation | QA (Douglas/Nilton)"], //wednesday
-    [8, "Daily Check-in | General project discussion | Documentation analysis | Test cases preparation"], //thursday
-    [8, "Daily Check-in | Search Refinement | Test cases preparation"]  //friday
+    [8, `${genericDescription}`], //monday
+    [8, `${genericDescription} | Specialized Retrospective Check-in`], //tuesday
+    [8, `${genericDescription}`], //wednesday
+    [8, `${genericDescription}`], //thursday
+    [8, `${genericDescription}`]  //friday
 ];
